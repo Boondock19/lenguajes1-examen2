641dad584dbbbc70cd3153aef2dcfb055a2ded10
Boondock19 <josematiasgonzalez20@gmail.com>
2022-07-10 01:14:29 -0400

    Creacion adecuada de tipos,structs y unions

--- 5/dataManager.js
+++ 5/dataManager.js
@@ -4,16 +4,13 @@
 
 
 
-console.log("Bienvenido al programa de diagramas de T")
+console.log("Bienvenido al programa de data Manager");
 console.log('Luego de cada accion, se pedira una siguiente')
 
 // Creando un array para cada uno de los programas a definir
-let arrayOfPrograms = [{
-    nombre: 'LOCAL',
-    lenguaje : 'LOCAL'
-}]
-let arrayOfInterpeters = []
-let arrayOfTranslaters = []
+let arrayOfAtomics = []
+let arrayOfStructs = []
+let arrayOfUnions = []
 
 
 
@@ -24,147 +21,129 @@ process.stdin.on('data', data => {
     const dataArray =dataString.split(" ")
     
 
-    if(dataArray[0].trim() == "DEFINIR") {
-        if (dataArray.length < 2) console.log("Recuerda especificar que tipo de programa se debe DEFINIR")
-        
-       if (dataArray[1] == "PROGRAMA") {
-        
-        if(dataArray.length < 4) {
-            console.log('Faltan argumentos para PROGRAMA')
+    /* 
+        Creacion de tipos atomicos
+    */
+    if(dataArray[0] == "ATOMICO") {
+        console.log("ENTRO EN ATOMICO")
+        if (dataArray.length < 3) { 
+            console.log("ERROR: Faltan datos")
         } else {
-            // Programa con nombre escrito en lenguaje 
-            const nombre = dataArray[2]
-            
-            let arrayFiltered = arrayOfPrograms.filter(x => x.nombre == nombre)
-            // Existe el nombre? 
-            
-            if (arrayFiltered.length > 0) {
-                console.log(`El  nombre ${nombre} ya posee un  programa asociado a el `)
+            let name = dataArray[1]
+            let representation = dataArray[2]
+            let alineacion = dataArray[3].trim()
+
+            const filteredName = arrayOfAtomics.filter (atomic => atomic.name == name) 
+            if (filteredName.length > 0) { 
+                console.log("ERROR: El nombre ya existe")
             } else {
-                const lenguaje = dataArray[3].trim()
-                const newProgram = {
-                    nombre,
-                    lenguaje
+                let newAtomic = {
+                    name,
+                    representation,
+                    alineacion
                 }
 
-
-                arrayOfPrograms.push(newProgram)
-
-                console.log(`Se definio el programa '${nombre}', ejecutable en '${lenguaje}' `)
+                arrayOfAtomics.push(newAtomic)
             }
         }
         
-            console.log("Siguiente accion: ")
-       }
-
-       if(dataArray[1] == "INTERPRETE") {
-
-            if(dataArray.length < 4) {
-                console.log('Faltan argumentos para INTERPRETE')
-            } else {
-                // Interprete escrito en lenguaje base para lenguaje
-                const lenguajeBase = dataArray[2]
-                const lenguaje = dataArray[3].trim()
-                const newInterpreter = {
-                    lenguajeBase,
-                    lenguaje
-                }
-
-                if (!existeInterpretador(arrayOfInterpeters,newInterpreter)) {
-                    arrayOfInterpeters.push(newInterpreter)
-                    console.log(`Se definio un interprete para  '${lenguaje}', escrito  en '${lenguajeBase}' `)
-                }
+        console.log(dataArray)
+        console.log(arrayOfAtomics)
+    }
         
+    
+
+        
+    
+
+    /* 
+        Creacion de structs
+    */
+    if(dataArray[0] == "STRUCT") {
+        console.log("ENTRA EN STRUCT")
+
+        if (dataArray.length < 3) { 
+            console.log("ERROR: Faltan datos")
+        } else {
+            let name = dataArray[1]
+            let arrayOfTypes = dataArray.slice(2)
+            arrayOfTypes[arrayOfTypes.length-1] = arrayOfTypes[arrayOfTypes.length-1].trim()
+            console.log(arrayOfTypes)
+            const filteredName = arrayOfStructs.filter (struct => struct.name == name) 
+            // Buscamos el nombre en los tipos, si existe no lo permitimos
+            let flagTypeName = findOne([name],arrayOfAtomics)
+            let flag = findOne(arrayOfTypes,arrayOfAtomics)
+            console.log(flag)
+            if (filteredName.length > 0) { 
+                console.log("ERROR: El nombre ya existe")
+
+            }
+            else if (flagTypeName != false) {
+                console.log("ERROR: El nombre ya existe y es de un tipo atomico")
+            }
+            else  if (flag == false) {
+                console.log("ERROR: un tipo no existe")
+            } else {
+                let newStruct = {
+                    name,
+                    arrayOfTypes
+                }
+
+                arrayOfStructs.push(newStruct)
             }
             
-            console.log("Siguiente accion: ")
         }
 
-        if(dataArray[1] == "TRADUCTOR") {
+        console.log(dataArray)
+        console.log(arrayOfStructs)
 
-            if(dataArray.length < 5) {
-                console.log('Faltan argumentos para TRADUCTOR ')
-            } else {
-                // Interprete escrito en lenguaje base para lenguaje
-                const lenguajeBase = dataArray[2]
-                const lenguajeOrigen = dataArray[3]
-                const lenguajeDestino = dataArray[4].trim()
-                const newTranslater = {
-                    lenguajeBase,
-                    lenguajeOrigen,
-                    lenguajeDestino
-                }
-
-                if (!existeTranslater(arrayOfTranslaters,newTranslater)) {
-                    arrayOfTranslaters.push(newTranslater)
-                    console.log(`Se definio un traductor para  '${lenguajeOrigen}' hacia '${lenguajeDestino}', escrito  en '${lenguajeBase}' `)
-                }
-            
-            }   
-           
-            console.log("Siguiente accion: ")
-        }
-        
     }
 
-    // Chequeo de ejcucion de un programa.
-    if(dataArray[0] == "EJECUTABLE") {
-        /**
-         * Solo pueden ser ejecutables si tienen
-         * una manera de llegar a lenguaje LOCAL bien sea 
-         * a traves de varios traductores o interpretes
-         */
 
-        const nombre = dataArray[1].trim()
+    /* 
+        Creacion de unions
+    */
+    if(dataArray[0] == "UNION") {
+        console.log("ENTRA EN UNION")
 
-        let arrayFiltered = arrayOfPrograms.filter(x => x.nombre == nombre)
-        // Existe el nombre? 
-
-
-        if( !(arrayFiltered.length > 0) ) {
-           return console.log(`El programa con el ${nombre} no ha sido declarado o definido`)
+        if (dataArray.length < 3) { 
+            console.log("ERROR: Faltan datos")
         } else {
+            let name = dataArray[1]
+            let arrayOfTypes = dataArray.slice(2)
+            arrayOfTypes[arrayOfTypes.length-1] = arrayOfTypes[arrayOfTypes.length-1].trim()
+            console.log(arrayOfTypes)
             
-            const programaEjecutar = arrayFiltered[0]
-            if (programaEjecutar.lenguaje === 'LOCAL') return console.log(`Si, es posible ejecutar el programa '${programaEjecutar.nombre}'`)
-            
-            //Buscamos los interpretes escritos para programaEjecutar.lenguaje
-            // Array con todos los interpretadores para programaEjecutar.lenguaje
-            const interpretadoresPrograma = arrayOfInterpeters.filter((interpreter) => interpreter.lenguaje === programaEjecutar.lenguaje)
-            const interpretadorLocal = {
-                lenguajeBase: "LOCAL",
-                lenguaje: programaEjecutar.lenguaje
+            const filteredName = arrayOfUnions.filter (union => union.name == name) 
+            // Buscamos el nombre en los tipos, si existe no lo permitimos
+            let flagTypeName = findOne([name],arrayOfAtomics)
+            let flag = findOne(arrayOfTypes,arrayOfAtomics)
+            console.log(flag)
+            if (filteredName.length > 0) { 
+                console.log("ERROR: El nombre ya existe")
+
             }
-
-            if(existeInterpretador(interpretadoresPrograma,interpretadorLocal)) return console.log(`Si, es posible ejecutar el programa '${programaEjecutar.nombre}'`)
-
-            //Buscamos los traductores escritos para programaEjecutar.lenguaje
-            // Array con todos los traductores con origen programaEjecutar.lenguaje
-            const traductoresPrograma = arrayOfTranslaters.filter((trans) => trans.lenguajeOrigen === programaEjecutar.lenguaje)
-
-            const traductorLocal =  {
-                lenguajeBase:"LOCAL",
-                lenguajeOrigen:programaEjecutar.lenguaje,
-                lenguajeDestino:"LOCAL"
+            else if (flagTypeName != false) {
+                console.log("ERROR: El nombre ya existe y es de un tipo atomico")
             }
+            else  if (flag == false) {
+                console.log("ERROR: un tipo no existe")
+            } else {
+                let newUnion = {
+                    name,
+                    arrayOfTypes
+                }
 
-
-            if(existeTranslater(traductoresPrograma,traductorLocal)) return console.log(`Si, es posible ejecutar el programa '${programaEjecutar.nombre}'`) 
-
-        
-            // Llamamos a la recuersion
+                arrayOfUnions.push(newUnion)
+            }
             
-            let esEjecutable = revisionProfunda(interpretadoresPrograma,traductoresPrograma)
-            
-            if(esEjecutable) return console.log(`Si, es posible ejecutar el programa '${programaEjecutar.nombre}'`)
-
         }
-        
-        console.log(`No, es posible ejecutar el programa '${nombre}'`)
-        console.log("Siguiente accion: ")
-    }
 
-   
+        console.log(dataArray)
+        console.log(arrayOfUnions)
+    }
+        
+    console.log("Siguiente accion: ")
 
     // Salimos del programa
     if (dataArray[0].trim() === 'SALIR') {
@@ -174,81 +153,22 @@ process.stdin.on('data', data => {
     
 });
 
-/**
-* 
-* @param {Objecto que representa una instacia de un interprete} interpreter 
-* @returns true si existe y false en caso contrario.
-*/
-const existeInterpretador = (arr,interpreter) => {
-return arr.some(inter => (inter.lenguaje === interpreter.lenguaje 
-    && inter.lenguajeBase === interpreter.lenguajeBase ))
-} 
 
 /**
-* 
-* @param {Objecto que representa una instacia de un traductor} trans
-* @returns true si existe y false en caso contrario.
-*/
-const existeTranslater = (arr,translater) => {
-return arr.some(trans => (trans.lenguajeOrigen === translater.lenguajeOrigen 
-    && trans.lenguajeDestino === translater.lenguajeDestino 
-    && trans.lenguajeBase === translater.lenguajeBase ))
-} 
-
-
-const revisionProfunda = (filtroInterpretadores,filtroTraductores) => {
-
-const interpretadorLocal = filtroInterpretadores.filter((interpretador) => interpretador.lenguajeBase === 'LOCAL')
-const traductorLocal = filtroTraductores.filter((traductor) => traductor.lenguajeBase === 'LOCAL')
-
-let newFiltroInterpretes = []
-let newFiltroTraductores = []
-
-let call;
-let call2
-
-
-if (interpretadorLocal.length > 0) {
-    return true
-} else {
-    filtroInterpretadores.forEach(element => {
-        arrayOfInterpeters.forEach(inter => {
-            if(element.lenguajeBase === inter.lenguaje) {
-                newFiltroInterpretes.push(inter)
-            }
-        })
+ * 
+ * @param {array con nombres a buscar} names 
+ * @param {array donde buscar los nombres} array 
+ * @returns bool que representa si lo encontro o no
+ */
+let findOne = (names,array) => {
     
-        arrayOfTranslaters.forEach(trans => {
-            if(element.lenguajeBase === trans.lenguajeOrigen) {
-                newFiltroTraductores.push(trans)
-            }
-        })
-        
-        call = revisionProfunda(newFiltroInterpretes,newFiltroTraductores)
-    });
+    for (let i = 0; i < names.length; i++) { 
+        let flag = array.find(element => element.name == names[i])
+        if (flag == undefined) {
+            return false
+        }
+    }
+   
 
-}
-
-if(traductorLocal.length > 0) {
     return true
-} else {
-    filtroTraductores.forEach(element => {
-        
-        arrayOfInterpeters.forEach(inter => {
-            if(element.lenguajeBase === inter.lenguaje) {
-                newFiltroInterpretes.push(inter)
-            }
-        })
-
-        arrayOfTranslaters.forEach(trans => {
-            if(element.lenguajeBase === trans.lenguajeOrigen) {
-                newFiltroTraductores.push(trans)
-            }
-        })
-
-        call2 = revisionProfunda(newFiltroInterpretes,newFiltroTraductores)
-    })
-}
-
-return (call || call2 )
 }
